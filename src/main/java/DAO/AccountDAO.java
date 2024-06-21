d835c0e1f807078900e16395e5fbb845413548d4
Spencer Williams <sdwilliams1122@gmail.com>
2024-06-21 03:45:29 +0000

    Got rid of some vital code during clean up, oops!

--- src/main/java/DAO/AccountDAO.java
+++ src/main/java/DAO/AccountDAO.java
@@ -14,7 +14,7 @@ public class AccountDAO {
         Connection connection = ConnectionUtil.getConnection();
         try {
             String sql = "insert into account(username, password) values (?, ?);" ;
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 
             preparedStatement.setString(1, account.getUsername());
             preparedStatement.setString(2, account.getPassword());
